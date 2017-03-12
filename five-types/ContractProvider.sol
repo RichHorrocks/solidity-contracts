035ade65750ef17bf1b4862deb74f627ac269276
Richard Horrocks <richardjhorrocks@gmail.com>
2017-03-12 18:07:55 +0000

    Addition of 'Five Types' contracts.

---
+++ five-types/ContractProvider.sol
@@ -0,0 +1,4 @@
+// Interface for getting contracts from Doug
+contract ContractProvider {
+    function contracts(bytes32 name) returns (address addr) {}
+}
---
+++ five-types/FundManagerEnabled.sol
@@ -0,0 +1,13 @@
+// Base class for contracts that only allow the fundmanager to call them.
+// Note that it inherits from DougEnabled
+contract FundManagerEnabled is DougEnabled {
+
+    // Makes it easier to check that fundmanager is the caller.
+    function isFundManager() constant returns (bool) {
+        if(DOUG != 0x0){
+            address fm = ContractProvider(DOUG).contracts("fundmanager");
+            return msg.sender == fm;
+        }
+        return false;
+    }
+}
