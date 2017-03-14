6f6ee07d046a5eab23a8a7a743f50b71b272c22e
Richard Horrocks <richardjhorrocks@gmail.com>
2017-03-14 20:19:11 +0000

    Addition of escrow contract.

---
+++ Escrow.sol
@@ -0,0 +1,78 @@
+contract Escrow {
+
+    mapping (address => uint) balances;
+
+    address public seller;
+    address public buyer;
+    address escrow;
+    bool sellerApprove;
+    bool buyerApprove;
+
+  // Constructor.
+  function Escrow {
+     escrow = msg.sender;
+  }
+
+  function setup (address seller, address buyer) {
+      if(msg.sender == escrow){
+          seller = seller;
+          buyer = buyer;
+      }
+  }
+
+  function approve(){
+      if (msg.sender == buyer) {
+          buyerApprove = true;
+      } else if (msg.sender == seller) {
+          sellerApprove = true;
+      }
+      
+      if (sellerApprove && buyerApprove) {
+          fee();
+      }
+  }
+
+  function abort() {
+      if (msg.sender == buyer) {
+          buyerApprove = false;
+      } else if (msg.sender == seller) {
+          sellerApprove = false;
+      }
+
+      if (!sellerApprove && !buyerApprove) {
+          refund();
+      }
+  }
+
+  function payOut(){
+      if (seller.send(this.balance)) {
+          balances[buyer] = 0;
+      }
+  }
+
+  function deposit(){
+      if(msg.sender == buyer) {
+          balances[buyer] += msg.value;
+      } else {
+          throw;
+      }
+  }
+
+  function killContract() internal {
+      selfdestruct(escrow);
+      //kills contract and returns funds to buyer
+  }
+
+  function refund(){
+      if (buyerApprove == false && sellerApprove == false) {
+          selfdestruct(buyer);
+      }
+    //send money back to recipient if both parties agree contract is void
+  }
+
+  function fee() {
+      escrow.send(this.balance / 100); //1% fee
+      payOut();
+  }
+
+}
